a3143369d104d5f2f432910f8bdf1651dd45fee8
Samuel Hegner <samuel.hegner@gmail.com>
2022-12-14 23:03:02 +0000

    Date changes

--- src/events/onInteraction.ts
+++ src/events/onInteraction.ts
@@ -1,13 +1,19 @@
 import { Interaction } from 'discord.js';
 import { CommandList } from '../commands/_CommandList';
 
-export const onInteraction = async (interaction: Interaction) => {
+export async function onInteraction(interaction: Interaction) {
 	if (interaction.isCommand()) {
+		console.log(`Interaction: ${interaction.commandName}`);
+
 		for (const Command of CommandList) {
 			if (interaction.commandName === Command.data.name) {
-				await Command.run(interaction);
+				try {
+					await Command.run(interaction);
+				} catch (error) {
+					console.error(`Error on Interaction ${interaction.commandName}: ${(error as Error).message}`);
+				}
 				break;
 			}
 		}
 	}
-};
+}
