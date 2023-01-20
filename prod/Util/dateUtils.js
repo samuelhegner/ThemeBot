637211a6b33f45483b5ce2222ccbf2397ddda4d2
Samuel Hegner <samuel.hegner@gmail.com>
2023-01-20 21:48:11 +0000

    Removing build from repo

--- prod/Util/dateUtils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDaysBetweenDates = void 0;
-function getDaysBetweenDates(date_1, date_2) {
-    let difference = date_1.getTime() - date_2.getTime();
-    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-    return Math.abs(TotalDays);
-}
-exports.getDaysBetweenDates = getDaysBetweenDates;
